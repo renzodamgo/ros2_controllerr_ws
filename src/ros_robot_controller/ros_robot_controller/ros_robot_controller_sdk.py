e293dd2d46f993a0f7470cf2a43cacd3ca28f2de
Renzo Damian <renzo.damian.go@gmail.com>
2024-11-19 19:47:55 +0000

    add gitignore

--- src/ros_robot_controller/ros_robot_controller/ros_robot_controller_sdk.py
+++ src/ros_robot_controller/ros_robot_controller/ros_robot_controller_sdk.py
@@ -96,7 +96,7 @@ class Board:
             'GAMEPAD_BUTTON_MASK_R1':        0x8000
     }
 
-    def __init__(self, device="/dev/rrc", baudrate=1000000, timeout=10):
+    def __init__(self, device="/dev/ttyACM0", baudrate=1000000, timeout=10):
         self.enable_recv = False
         self.frame = []
         self.recv_count = 0
